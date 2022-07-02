559bc452a3eecca1f5e2fe5afe6d1c721c98e55a
Fredrik Uddsten <Fredrik.Uddsten1@ey.com>
2022-07-02 04:58:06 -0400

    WIP - Scorer class added

--- arboretum/config.py
+++ arboretum/config.py
@@ -1,13 +1,42 @@
 BOARD_ROWS = 8
 BOARD_COLUMNS = 10
 DECK_SHORTHANDS = {
-	"Oak 1":"[O01]",
-	"Oak 2":"[O02]",
-	"Oak 3":"[O03]",
-	"Oak 4":"[O04]",
-	"Oak 5":"[O07]",
-	"Oak 6":"[O08]",
-	"Oak 7":"[O09]",
-	"Oak 8":"[O10]",
-	"Oak 9":"[O11]",
+	"Oak 1":"[O1]",
+	"Oak 2":"[O2]",
+	"Oak 3":"[O3]",
+	"Oak 4":"[O4]",
+	"Oak 5":"[O6]",
+	"Oak 6":"[O6]",
+	"Oak 7":"[O7]",
+	"Oak 8":"[O8]",
+	"Cassia 1":"[C1]",
+	"Cassia 2":"[C2]",
+	"Cassia 3":"[C3]",
+	"Cassia 4":"[C4]",
+	"Cassia 5":"[C5]",
+	"Cassia 6":"[C6]",
+	"Cassia 7":"[C7]",
+	"Cassia 8":"[C8]",
+	"Dogwood 1":"[D1]",
+	"Dogwood 2":"[D2]",
+	"Dogwood 3":"[D3]",
+	"Dogwood 4":"[D4]",
+	"Dogwood 5":"[D5]",
+	"Dogwood 6":"[D6]",
+	"Dogwood 7":"[D7]",
+	"Dogwood 8":"[D8]",
+	"Jacaranda 1":"[J1]",
+	"Jacaranda 2":"[J2]",
+	"Jacaranda 3":"[J3]",
+	"Jacaranda 4":"[J4]",
+	"Jacaranda 5":"[J5]",
+	"Jacaranda 6":"[J6]",
+	"Jacaranda 7":"[J7]",
+	"Jacaranda 8":"[J8]",
 }
+
+TREES = ["Cassia", "Dogwood", "Jacaranda", "Oak"]
+# TODO - add in aditional trees below to use when more than 2 players
+# ["Lilac", "Magnolia", "Maple", "Olive"]
+CARDS_PER_TREE_TYPE = 8
+NUM_CARDS_STARTING_HAND = 7
--- arboretum/tests/test_init.py
+++ arboretum/tests/test_init.py
@@ -1,5 +1,6 @@
 import pytest
-from arboretum import Board, Player, Deck, config
+import pickle
+from arboretum import Board, Player, Deck, Scorer, config
 
 @pytest.fixture
 def board():
@@ -11,6 +12,10 @@ def deck():
 
 @pytest.fixture
 def player(board, deck):
+	return Player(deck, board)
+
+@pytest.fixture
+def player2(board, deck):
 	return Player(deck, board)
 
 def test_create_board_grid(board):
@@ -53,3 +58,72 @@ def test_remove_drawn_card_from_deck(player):
 	player.draw_card()
 	assert card_to_draw not in player.deck.cards 
 
+def test_check_if_occupied_loc(board):
+	row = 1
+	column = 1
+	check_occupied = board._check_if_occupied_loc(row, column)
+	assert (check_occupied == False)
+	board.board_grid[row][column] = "Oak 1"
+	check_occupied = board._check_if_occupied_loc(row, column)
+	assert (check_occupied == True)
+
+def test_reject_no_adjacency(player):
+	tree = "Oak 2"
+	row, column = 0, 1
+	player.first_tree_placed = True
+	with pytest.raises(ValueError):
+		player.place_tree(tree, row, column)
+
+def test_reject_no_adjacency_diagnoal_tree(player):
+	tree1 = "Oak 1"
+	tree2 = "Oak 2"
+	row1, column1 = 0, 0
+	row2, column2 = 1,1
+	player.place_tree(tree1, row1, column1)
+	with pytest.raises(ValueError):
+		player.place_tree(tree2, row2, column2)
+
+def test_shuffle_deck(deck):
+	pre_shuffle_pickle = pickle.dumps(deck.cards)
+	deck.shuffle_deck()
+	post_shuffle_pickle = pickle.dumps(deck.cards)
+	assert pre_shuffle_pickle != post_shuffle_pickle
+
+def test_establish_scorer_simple_case(player, player2):
+	player.trees_on_hand = ["Oak 2", "Oak 3", "Cassia 5", "Dogwood 6", "Dogwood 2", "Dogwood 3", "Jacaranda 5"]
+	player2.trees_on_hand = ["Oak 8", "Cassia 8", "Dogwood 4", "Dogwood 5", "Jacaranda 7"]
+	trees = ["Cassia", "Dogwood", "Jacaranda", "Oak"]
+	scorer_dict = Scorer.calculate_scorer([player, player2], trees)
+	assert scorer_dict["Cassia"] == [player2]
+	assert scorer_dict["Dogwood"] == [player]
+	assert scorer_dict["Jacaranda"] == [player2]
+	assert scorer_dict["Oak"] == [player2]
+
+def test_establish_scorer_both_score(player, player2):
+	player.trees_on_hand = ["Oak 2", "Oak 3", "Cassia 5", "Cassia 2"]
+	player2.trees_on_hand = ["Oak 5", "Cassia 7"]
+	trees = ["Cassia", "Oak"]
+	scorer_dict = Scorer.calculate_scorer([player, player2], trees)
+	assert player1 in scorer_dict["Cassia"]
+	assert player2 in scorer_dict["Cassia"]
+	assert player1 in scorer_dict["Oak"]
+	assert player2 in scorer_dict["Oak"]
+
+def test_establish_scorer_no_one_scores(player, player2):
+	player.trees_on_hand = ["Oak 1", "Oak 2", "Oak 3"]
+	player2.trees_on_hand = []
+	trees = ["Cassia", "Dogwood", "Jacaranda", "Lilac", "Magnolia", "Maple", "Olive"]
+	scorer_dict = Scorer.calculate_scorer([player, player2], trees)
+	assert scorer_dict["Cassia"] == None
+	assert scorer_dict["Dogwood"] == None
+	assert scorer_dict["Jacaranda"] == None
+	assert scorer_dict["Lilac"] == None
+	assert scorer_dict["Magnolia"] == None
+	assert scorer_dict["Maple"] == None
+	assert scorer_dict["Olive"] == None
+
+def test_establish_scorer_8_vs_1(player, player2):
+	player.trees_on_hand = ["Oak 1"]
+	player2.trees_on_hand = ["Oak 8"]
+	scorer_dict = Scorer.calculate_scorer([player, player2], trees)
+	assert scorer_dict["Oak"] == player
